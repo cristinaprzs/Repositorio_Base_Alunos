4542bbfc85e464a44feec7083347748e2f60b512
cristinaprzs <cristinaprz076@gmail.com>
2025-09-15 10:17:49 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/tabuada2.py
@@ -0,0 +1,4 @@
+número = int(input("Digite o número requerido: "))
+
+for i  in range(1, 11):
+    print(f" {i} x {número} = {i * número}")
---
+++ Modulo 1/Loopings/tabuada3.py
@@ -0,0 +1,5 @@
+numero = 5
+i  = 0
+
+while i <= 10:
+    print(f"{i} * {numero} = {i * numero}")
